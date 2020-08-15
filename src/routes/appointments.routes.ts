be257ebff783a2c0d1f28df67bf1da3c6a62654b
todd7 <edson_a@outlook.com>
2020-08-14 23:41:52 -0300

    added models and repositories

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const AppointmentsRepository = new AppointmentRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = AppointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = AppointmentsRepository
+    .findByDate(parsedDate);
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = AppointmentsRepository.create(provider, parsedDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { json } from 'express';
+
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello Edson' });
-});
+app.use(json());
+
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
